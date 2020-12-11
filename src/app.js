738f4d116ef620d4ba4e467594dedb30fd3430d6
Paul Barnette <paul.barnette@gmail.com>
2020-12-11 16:54:13 +1100

    Update of the app to take in data for the grid size from an external file

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 #!/usr/bin/env node
 
-const validation = require('./util/validation.js')
-const chalk = require('chalk');
-require('./util/messaging')
+"use strict";
+
+/* Enviro variables */
+const config = require('../config.json');
+const defaultConfig = config.development;
+const environment = process.env.NODE_ENV || 'development';
+
+/* Objects brought into the project */
+const validation = require('./util/validation.js');
 let Grid = require('./components/Grid');
 let Messaging = require('./util/messaging.js');
+const Help = require('./util/Help');
+const EventEmitter = require('events').EventEmitter;
+
 
 /**
  * To receive input command from user continuously, until user explicity terminate the program
@@ -30,11 +39,23 @@ const init = () => {
   //explainer.printHeader();
   //explainer.printDescription();
 
-    console.log(Grid.sizeStr())
-    console.log(Grid.dimensions())
+    //console.log(Grid.sizeStr())
+    //console.log(Grid.dimensions())
 
-  Messaging.announce(Grid.sizeStr())  
-  readInput();
+      
+    // create a new grid based on the configuration
+    //console.log(new Grid.Grid(defaultConfig.grid[0], defaultConfig.grid[1]))
+    const grid = new Grid.Grid(defaultConfig.grid[0], defaultConfig.grid[1]);
+
+
+    // pubsub call for letting the user know about the grid
+    Messaging.emit("normal",grid.sizeStr());  
+    
+    // Calls program help
+    Help.displayHelp()
+
+    console.log(defaultConfig)
+    readInput();
 };
 
 init();
